28e98591919642f580baf83639a005ffcc945be3
KarlPollock91 <56038901+KarlPollock91@users.noreply.github.com>
2019-10-04 15:05:53 +1300

    Add files via upload

--- TetrUC/lab1-ex5.c
+++ TetrUC/lab1-ex5.c
@@ -11,8 +11,8 @@
 
 
 typedef struct point {
-    uint8_t x;
-    uint8_t y;
+    int8_t x;
+    int8_t y;
 } Point;
 
 typedef struct tetronimo_type_s {
@@ -51,6 +51,12 @@ static Tetromino J_block = {{{{0, 0}, {-1, 0}, {0, 1}, {0, 2}},
                         {{0, 0}, {0, 1}, {1, 0}, {2, 0}},
                         {{0, 0}, {1, 0}, {0, -1}, {0, -2}},
                         {{0, 0}, {-1, 0}, {-2, 0}, {0, -1}}}, 4};
+
+static Tetromino S_block = {{{{0, 0}, {-1, 0}, {0, 1}, {1, 1}},
+                        {{0, 0}, {0, -1}, {-1, 0}, {-1, 1}}}, 2};
+
+static Tetromino Z_block = {{{{0, 0}, {0, 1}, {0, 1}, {-1, 1}},
+                        {{0, 0}, {0, -1}, {1, 0}, {1, 1}}}, 2};
 
 static pio_t ledmat_rows[] =
 {
@@ -97,8 +103,11 @@ void clear_screen(void)
     }
 }
 
+
+//Old way of checking if game should end
 //Checks to see if the tetrominos have reached the top and the game should end
-bool is_game_over(bool board[7][5])
+
+/*bool is_game_over(bool board[7][5])
 {
     bool game_over = false;
     for (int i = 0; i < 4; i++) {
@@ -108,12 +117,13 @@ bool is_game_over(bool board[7][5])
     }
     return game_over;
 }
+*/
 
 //Check to see if the space below the current block is clear. Return true if so.
 bool can_drop(Cursor* cursor, bool board[7][5])
 {
     uint8_t x;
-    uint8_t y;
+    int8_t y;
 
     bool success = true;
     for (int i = 0; i < 4; i++) {
@@ -121,7 +131,7 @@ bool can_drop(Cursor* cursor, bool board[7][5])
         y = (cursor->tetromino.blocks[cursor->rotation][i].y + cursor->y);
         if (y == 6) {
             success = false;
-        } else if (board[y+1][x]) {
+        } else if ((y > 0) && board[y+1][x]) {
             success = false;
         }
     }
@@ -172,13 +182,13 @@ void reset_tcnt1(void)
 //Sets the cursor to be a new tetromino and tries to place it at the top of the screen.
 void generate_new_tetromino(Cursor* cursor)
 {
-    Tetromino possible_tetrominos[5] = {T_block, I_block, square_block, L_block, J_block};
-    uint8_t random_number = rand() % 5;
+    Tetromino possible_tetrominos[7] = {T_block, I_block, square_block, L_block, J_block, S_block, Z_block};
+    uint8_t random_number = rand() % 7;
 
     cursor->x = 2;
     cursor->y = 0;
     cursor->tetromino = possible_tetrominos[random_number];
-    cursor->rotation = 1;
+    cursor->rotation = 0;
 
     /*TODO: Once we've implimented a push left/right function we can decide whether or not
      * we can place the block and cause a gameover if not.
@@ -202,6 +212,97 @@ void set_tetromino(Cursor* cursor, bool board[7][5])
     }
 }
 
+
+
+
+
+//Check if it can move left if direction is -1, right if direction is -1, or if it can stay still if direction is 0.
+bool check_sides(Cursor* cursor, bool board[7][5], int8_t direction)
+{
+    uint8_t x;
+    int8_t y;
+
+    bool success = true;
+    for (int i = 0; i < 4; i++) {
+        x = (cursor->tetromino.blocks[cursor->rotation][i].x + cursor->x);
+        y = (cursor->tetromino.blocks[cursor->rotation][i].y + cursor->y);
+        if (((x + direction) < 0) || ((x + direction) > 4)) {
+            success = false;
+        } else if ((y >= 0) && board[y][x + direction]) {
+            success = false;
+        }
+    }
+    return success;
+}
+
+//Check to see if a line has been completed and clear it.
+void line_clear(bool board[7][5]) {
+
+    bool cleared = false;
+
+    for (int i = 0; i < 7; i++) {
+        cleared = true;
+        for (int j = 0; j < 5; j++) {
+            if (!board[i][j]) {
+                cleared = false;
+            }
+        }
+        if (cleared) {
+            //TODO: ADD POINTS OR PERFORM MULTIPLAYER ACTION
+
+            //shift all rows above cleared row downwards
+            for (int j = i; j > 0; j--) {
+                for (int k = 0; k < 5; k++) {
+                    board[j][k] = board[j-1][k];
+                }
+            }
+            //and set top row to be empty
+            for (int k = 0; k < 5; k++) {
+                board[0][k] = false;
+            }
+        }
+    }
+}
+
+//Checks for user input.
+void cursor_control(Cursor* cursor, bool board[7][5])
+{
+     if (navswitch_push_event_p(NAVSWITCH_SOUTH)) {
+         if (can_drop(cursor, board)){
+             cursor->y += 1;
+         } else {
+             set_tetromino(cursor, board);
+         }
+    } else if (navswitch_push_event_p(NAVSWITCH_NORTH)) {
+        //rotate
+        cursor->rotation = (cursor->rotation + 1) % cursor->tetromino.num_rotations;
+        if (!check_sides(cursor, board, 0)) {
+            //if there is no room to rotate in place
+            if (check_sides(cursor, board, -1)) {
+                //keep rotation and push block to the left if there's space
+                cursor->x -= 1;
+            } else if (check_sides(cursor, board, 1)) {
+                //keep rotation and push block to the right if there's space
+                cursor->x += 1;
+            } else {
+                //unable to rotate, cancel rotation.
+                cursor->rotation = (cursor->rotation - 1) % cursor->tetromino.num_rotations;
+            }
+        }
+    } else if (navswitch_push_event_p(NAVSWITCH_EAST)) {
+        //move right
+        if (check_sides(cursor, board, 1)) {
+            cursor->x += 1;
+        }
+    } else if (navswitch_push_event_p(NAVSWITCH_WEST)) {
+        //move left
+        if (check_sides(cursor, board, -1)) {
+            cursor->x -= 1;
+        }
+    }
+}
+
+
  /* your equivalent of a print statement
     pio_output_low(ledmat_cols[2]);
     pio_output_low(ledmat_rows[2]);
@@ -219,7 +320,13 @@ int main (void)
     //Initialisation stuff
     system_init ();
     led_matrix_init();
-    srand(TCNT1);
+    navswitch_init();
+
+    //Seed change, will be different depending on how long you spend on title screen
+    //srand(TCNT1);
+
+    //Testing srand, set to whatever you want if you get sick of the same bricks over and over
+    srand(2409);
 
     initialise_board(board);
     navswitch_init();
@@ -259,12 +366,25 @@ int main (void)
             } else {
                 //Generate new tetromino and check for game over.
                 set_tetromino(&cursor, board);
+                line_clear(board);
                 generate_new_tetromino(&cursor);
-                game_over = is_game_over(board);
+                //game_over = is_game_over(board);
+                if (!check_sides(&cursor, board, 0)) {
+                    if (check_sides(&cursor, board, -1)) {
+                        cursor.x -= 1;
+                    } else if (check_sides(&cursor, board, 1)) {
+                        cursor.x += 1;
+                    } else {
+                        game_over = true;
+                    }
+                }
                 reset_tcnt1();
             }
             ready_to_drop = 0;
         }
+
+        navswitch_update();
+        cursor_control(&cursor, board);
 
         //Controls when to drop the tetromino a row.
         if ((TCNT1 % mark) > (mark / 2) && toggle_ready) {
